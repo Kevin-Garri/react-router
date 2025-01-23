c8f7a4ecd465c37a7e7b140da9d6788a1ca356b2
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-01-23 13:42:13 +0100

    fit: import in appjsx le pages e il router dom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+//pages
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
+import ChiSiamo from "./pages/ChiSiamo"
+import ListaPost from "./pages/listaPost"
+
+
+
+function App() {
+
+
+  return
+
+}
+
+export default App
